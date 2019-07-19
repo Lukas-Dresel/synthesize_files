1ff4429ea35ec4c8717bf60cd1db4d0b560aae15
Lukas-Dresel <Lukas-Dresel@users.noreply.github.com>
2019-07-18 21:05:23 -0700

    made pngs more usable

--- synthesize_files/synthetic_png.py
+++ synthesize_files/synthetic_png.py
@@ -13,6 +13,7 @@ COLOR_TYPE = {
 def make_png_chunk(name, data):
     return p32(len(data)) + name + data + p32(zlib.crc32(name + data))
 
+
 def make_ihdr_data(width, height, bit_depth=8, color_type='rgb', comp_meth=0, filt_meth=0, inter_meth=0):
     d = p32(width) + p32(height)
     d += p8(bit_depth) + p8(COLOR_TYPE[color_type]) 
@@ -22,16 +23,12 @@ def make_ihdr_data(width, height, bit_depth=8, color_type='rgb', comp_meth=0, fi
     assert len(d) == 13
     return d
 
-def malicious_IDAT_chunk():
-    assert False
 
-def make_png(image_data, width=100, height=100):
+def make_png(chunks, width=100, height=100):
     d = b''
     d += bytes([137, 80, 78, 71, 13, 10, 26, 10]) # magic
-    
-    chunks = [
-        (IHDR, make_ihdr_data(width, height)),
-        (IDAT, image_data),
-        (IEND, b''),
-    ]
+
+    chunks = [(IHDR, make_ihdr_data(width, height))]
+    chunks += [(IDAT, c) for c in chunks]
+    chunks += [(IEND, b'')]
     return d + b''.join(make_png_chunk(v[0], v[1]) for v in chunks)
